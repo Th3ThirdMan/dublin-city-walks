b56e62852d8cc2c213750cff116f05ead32b79f6
Th3ThirdMan <davidgkennedymail@gmail.com>
2022-02-12 12:01:21 +0000

    Add JS code for Hamburger responsiveness

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementsByClassName('nav-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+
+toggleButton.addEventListener('click', () => {
+    navLinks.classList.toggle('active')
+})
